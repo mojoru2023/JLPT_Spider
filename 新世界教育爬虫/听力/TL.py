8f863a186bef16ac68b0d27154f745f81a4bbda0
Greenbirch2007 <291109028@qq.com>
2019-03-01 15:39:53 +0800

    新世界教育关于日语的爬虫

---
+++ 新世界教育爬虫/听力/TL.py
@@ -0,0 +1,81 @@
+import datetime
+import re
+import time
+
+import pymysql
+import requests
+from lxml import etree
+from requests.exceptions import RequestException
+
+def call_page(url):
+    req= requests.get(url)
+      #  requests 中文编码的终极办法！
+    if req.encoding == 'ISO-8859-1':
+        encodings = requests.utils.get_encodings_from_content(req.text)
+        if encodings:
+            encoding = encodings[0]
+        else:
+            encoding = req.apparent_encoding
+
+        # encode_content = req.content.decode(encoding, 'replace').encode('utf-8', 'replace')
+        global encode_content
+        encode_content = req.content.decode(encoding, 'replace')  # 如果设置为replace，则会用?取代非法字符；
+        return  encode_content
+
+
+
+# 正则和lxml混用
+def parse_html(html):  # 正则专门有反爬虫的布局设置，不适合爬取表格化数据！
+    big_list = []
+    selector = etree.HTML(html)
+    title = selector.xpath("/html/body/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/div[1]/a/text()")
+    links = selector.xpath('/html/body/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/div[1]/a/@href')
+
+    for i1,i2 in zip(title,links):
+        big_list.append((i1,i2))
+
+    return big_list
+
+
+
+
+
+
+
+
+def insertDB(content):
+    connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='JLPT',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+    cursor = connection.cursor()
+    try:
+        cursor.executemany('insert into XIJJY_tingli (title,link) values (%s,%s)', content)
+        connection.commit()
+        connection.close()
+        print('向MySQL中添加数据成功！')
+    except TypeError :
+        pass
+
+if __name__=="__main__":
+    for num in range(1,36):
+        print(num)
+        url = "https://jp.xsjedu.org/Topic/tingli/" + str(num) + "/"
+        html = call_page(url)
+        content = parse_html(html)
+        insertDB(content)
+        time.sleep(0.5)
+
+
+
+
+
+
+
+# create table XIJJY_tingli(
+# id int not null primary key auto_increment,
+# title varchar(50),
+# link varchar(50)
+# ) engine=InnoDB  charset=utf8;
+
+
+# drop table XIJJY_tingli;
